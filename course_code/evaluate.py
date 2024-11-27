d5283e8c826a1e1b95a06f4194c29da57b0da0c5
MichaelJSr <miky.srouji@gmail.com>
2024-11-27 15:39:28 -0800

    added additional data paths for crag data

--- course_code/evaluate.py
+++ course_code/evaluate.py
@@ -74,7 +74,8 @@ if __name__ == "__main__":
     parser.add_argument("--dataset_path", type=str, default="example_data/dev_data.jsonl.bz2",
                         choices=["example_data/dev_data.jsonl.bz2", # example data
                                  "data/crag_task_1_dev_v4_release.jsonl.bz2", # full data
-                                 "cs245-project-group-p3/example_data/dev_data.jsonl.bz2", # example data
+                                 "cs245-project-group-p3/example_data/dev_data.jsonl.bz2",
+                                 "cs245-project-group-p3/data/crag_task_1_dev_v4_release.jsonl.bz2",
                                  ])
 
     parser.add_argument("--model_name", type=str, default="vanilla_baseline",
