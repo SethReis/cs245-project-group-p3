ee467a6c87614a1cc007eab606078dbbc639f257
Edward Hwang <hwang.edward8@gmail.com>
2024-11-28 03:50:38 +0000

    evaluation results using the example data
    * runs much faster, probably helpful to evaluate our approaches

--- course_code/run_evaluate.sh
+++ course_code/run_evaluate.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 python3 evaluate.py \
-    --dataset_path "data/crag_task_1_dev_v4_release.jsonl.bz2" \
+    --dataset_path "example_data/dev_data.jsonl.bz2" \
     --model_name "vanilla_baseline" \
     --llm_name "meta-llama/Llama-3.2-1B-Instruct" \
     --is_server \
